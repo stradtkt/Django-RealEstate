d15fdf468ff0be2644d8c0fdf54fd6a29d0cfe04
Kevin Stradtman <stradtkt22@gmail.com>
2020-01-03 16:44:36 -0500

    Added models for realtors and listings

--- untitled/settings.py
+++ untitled/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'untitled.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btre_db',
+        "USER": 'postgres',
+        "PASSWORD": 'root',
+        "HOST": 'localhost',
     }
 }
 
